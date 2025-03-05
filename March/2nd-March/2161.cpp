991196ab65e53f27f21fa256383fd1fe7eb278c2
Mitesh Agarwal <mitu24100@gmail.com>
2025-03-04 22:15:39 -0500

    Added 2nd March Solution

---
+++ March/2nd-March/2161.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> less,equal,greater;
+        for(int num: nums){
+            if(num < pivot){
+                less.push_back(num);
+            }
+            else if(num == pivot){
+                equal.push_back(num);
+            }
+            else{
+                greater.push_back(num);
+            }
+        }
+        for(int num:equal){
+            less.push_back(num);
+        }
+        for(int num:greater){
+            less.push_back(num);
+        }
+
+        return less;
+    }
+};
+
+int main(){
+    Solution s;
+    std::cout<<s.pivotArray({-3,4,3,2}, 2)<<endl;
+}
